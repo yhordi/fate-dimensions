c21ed28c0d5e8dfa5ac2b883e8b2582df76c6ea3
Jordan Kamin <jordanakamin@gmail.com>
2017-02-07 12:24:24 -0800

    adds routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, except: :index
+  resources :sessions, only: [:create, :destroy, :show]
+  resources :systems do
+    resources :skills
+    resources :npcs
+  end
+  resources :npcs, shallow: true do
+    resources :character_skills
+    resources :stunts
+    resources :consequences, only: [:create, :destroy, :update]
+  end
+  resources :aspects, except: [:index, :show]
+  root 'users#new'
 end

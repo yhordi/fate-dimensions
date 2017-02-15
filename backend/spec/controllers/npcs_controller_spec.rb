28b1d374d943f3ef0c7ed295994cd3d8e31a5d8f
Jordan Kamin <jordanakamin@gmail.com>
2017-02-15 14:48:04 -0800

    Adds tests for NpcsController#create

--- backend/spec/controllers/npcs_controller_spec.rb
+++ backend/spec/controllers/npcs_controller_spec.rb
@@ -1,20 +1,22 @@
-# describe NpcsController do
-#   let(:npc) { FactoryGirl.create :npc }
-#   let(:strength) { CharacterSkill.create(name: "Strength", level: 3)}
-#   let(:will) { CharacterSkill.create(name: "Will", level: 3)}
-#   describe "#create" do
-#     before(:each) do
-#       npc.character_skills << strength
-#       npc.character_skills << will
-#     end
-#     xit 'saves a new Npc in the database' do
-#     end
-#     xit 'renders json to the view' do
-#     end
-#     it "adjusts an Npc's max physical stress based on the will skill" do
+describe NpcsController do
+  let(:user) { FactoryGirl.create :user}
+  let(:system) { FactoryGirl.create :system, user_id: user.id}
+  let(:npc) { FactoryGirl.create :npc, system_id: system.id }
+  let(:new_npc) { FactoryGirl.build :npc }
+  let(:strength) { CharacterSkill.create(name: "Strength", level: 3)}
+  let(:will) { CharacterSkill.create(name: "Will", level: 3)}
+  describe "#create" do
+    before(:each) do
+      npc.character_skills << strength
+      npc.character_skills << will
+      post :create, params: {npc: new_npc.attributes}
+    end
+    it 'saves a new Npc in the database' do
+      expect(Npc.last.name).to eq(new_npc.name)
+    end
+    it 'renders json with npc info to the client' do
+      expect(response.body).to include(new_npc.name)
+    end
 
-#     end
-#     it "adjust an Npc's max physical stress based on the physique skill" do
-#     end
-#   end
-# end
+  end
+end

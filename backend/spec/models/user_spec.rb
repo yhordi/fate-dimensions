20fdd67fa6c8b57538fe3c238da1b4010da830da
Jordan Kamin <jordanakamin@gmail.com>
2017-02-15 14:35:08 -0800

    sets up simplecov

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -3,10 +3,6 @@ describe User do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_uniqueness_of :name }
     it { is_expected.to have_secure_password }
-    it { is_expected.to have_attached_file(:avatar)}
-    it { is_expected.to validate_attachment_content_type(:avatar).
-      allowing('image/png', 'image/gif', 'image/jpg').
-      rejecting('text/plain', 'text/xml') }
   end
   describe "associations" do
     it { is_expected.to have_many :systems }
